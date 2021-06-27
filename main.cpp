3368219254d2587fc00ba6112078f3900ba85b61
williamsimms <williamsimms193@gmail.com>
2021-06-27 15:19:47 -0500

    fixes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <functional>
-#include <iomanip>
 #include <ios>
 #include <iostream>
 #include <string>
@@ -7,41 +6,10 @@
 #include <vector>
 
 #include "hashtable.h"
-#include "linkedlist.h"
-#include "utility/Vector3.hpp"
 
 using namespace std;
 
 int main() {
-  cout << boolalpha;
-
-  HashTable<string, Vector3> ht;
-
-  // int x = 5;
-
-  // ht.Insert("William", x);
-  // ht.Insert("Javier", x);
-  // ht.Insert("Xavier", x);
-  // ht.Insert("Anthony", x);
-  // ht.Insert("s", x);
-  // ht.Insert("Arthdur", x);
-  // ht.Insert("Arthur", x);
-  // ht.Insert("Arthudr", x);
-  // ht.Insert("Arthussfdgr", 2);
-
-  // cout << ht["William"];
-
-  // ht.Insert("Arthur", x);
-  // ht.Insert("Arthusrd", x);
-  // ht.Insert("Arthusr", x);
-  // ht.Insert("Arthudr", x);
-  // ht.Insert("Arthudr", x);
-
-  //  unordered_map<string, int> map;
-  // map["Yello"] = 4;
-  // cout << map["Yello"] << endl;
-
-  cout << ht["Yello"];
-
+  HashTable<string, int> map;
   return 0;
 }
--- test/hashtable.test.cpp
+++ test/hashtable.test.cpp
@@ -1,8 +1,58 @@
 #define CATCH_CONFIG_MAIN
-#include "../hashtable.h"
 
+#include "../hashtable.h"
+#include "../utility/Vector3.hpp"
 #include "../vendor/catch.hpp"
 
-TEST_CASE("", "[Insert]") {
+TEST_CASE("", "[HashTable()]") {
   //
+}
+
+TEST_CASE("", "[HashTable(int)]") {
+  //
+}
+
+TEST_CASE("", "[HashTable(const HashTable<K,V,H>&)]") {
+  //
+}
+
+TEST_CASE("", "[HashTable(HashTable<K,V,H>&&)]") {
+  //
+}
+
+TEST_CASE("", "[operator=]") {
+  //
+}
+
+TEST_CASE("", "[Empty]") {
+  //
+}
+
+TEST_CASE("", "[Size]") {
+  //
+}
+
+TEST_CASE("", "[Clear]") {
+  //
+}
+
+TEST_CASE("Inserts a New Element to the Hash Table.",
+          "[Insert(const K& key, const V& value)]") {
+  SECTION("Inserts a new Element to the HashTable.") {
+    //
+  }
+}
+
+TEST_CASE("Inserts a New Element to the Hash Table.",
+          "[Insert(const K& key, V&& value)]") {
+  SECTION("Inserts a new Element to the HashTable using L Value.") {
+    //
+  }
+}
+
+TEST_CASE("Emplaces a new Element into the hashtable.",
+          "[Emplace(const K&, Args&&...)]") {
+  SECTION("Inserts a new Element to the HashTable using L Value.") {
+    //
+  }
 }
